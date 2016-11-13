0c451901de1e2fddbcdf6bdc1397d0ea1dc04480
269219362ljf <269219362ljf@gmail.com>
2016-11-13 22:57:27 +0800

    完成解释xmlutil

--- src/test/test/XmlTest.java
+++ src/test/test/XmlTest.java
@@ -1,9 +1,11 @@
 package test;
 
+import config.XmlConfig;
 import org.json.JSONObject;
 import utils.XmlUtil;
 
 import java.io.File;
+import java.util.Set;
 
 /**
  * Created by lu on 2016/11/13.
@@ -14,7 +16,6 @@ public class XmlTest {
         XmlUtil util=new XmlUtil();
         XmlTest test=new XmlTest();
         String xmlFile="test.xml";
-        File directory = new File("..");
         String path=System.getProperty("user.dir");
         xmlFile=test.getPath()+xmlFile;
         System.out.println(xmlFile);
@@ -27,14 +28,26 @@ public class XmlTest {
             System.out.println("fail");
             return;
         }
-
         JSONObject jsonTest=util.xml2JSONObject(file);
+        System.out.println(jsonTest);
 
-
-
-
+        XmlConfig config=XmlConfig.getInstance();
+        jsonTest=config.getXMLconfig("paramconfig.xml");
+        System.out.println("*****************");
+        System.out.println(jsonTest);
+        System.out.println("*****************");
+        if(config.isNodeInXml("paramconfig.xml","test-value1")){
+            System.out.println("*****************");
+            System.out.println("node exists");
+            System.out.println("*****************");
+        }else{
+            System.out.println("*****************");
+            System.out.println("node not exists");
+            System.out.println("*****************");
+        }
 
     }
+
     //获取当前类的路径（编译后）
     public String getPath(){
         return this.getClass().getResource("").getPath();
