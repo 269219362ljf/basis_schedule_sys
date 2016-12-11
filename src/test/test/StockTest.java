80b3c5e64c5a2b354f1605f0c63abaea1f585063
269219362ljf <269219362ljf@gmail.com>
2016-12-11 23:20:09 +0800

    增加股票测试类，获取股票代码成功

--- src/test/test/StockTest.java
+++ src/test/test/StockTest.java
@@ -1,9 +1,12 @@
 package test;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by lu on 2016/12/11.
@@ -31,8 +34,86 @@ public class StockTest {
             }
         }
 
+    // 获取新浪38也的所有股票代码
+    private static List<String> getAllStackCodes() throws IOException {
+        List<String> codes = new ArrayList<String>() ;
+        int i =1 ;
+        URL url = null ;
+        // 新浪 股票 好像目前为止就 38页
+        //更新共47
+        for(; i < 48 ; i ++ ){
+            url = new URL("http://vip.stock.finance.sina.com.cn/q/go.php/vIR_CustomSearch/index.phtml?p="+i) ;
+            String code = getBatchStackCodes(url) ;
+            codes.addAll(handleStockCode(code)) ;
+        }
+
+        return codes ;
+    }
+
+
+
+
+    //   返回的值是一个js代码段  包括指定url页面包含的所有股票代码
+    public static String getBatchStackCodes(URL url) throws IOException{
+        URLConnection connection = url.openConnection() ;
+        connection.setConnectTimeout(30000) ;
+        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream())) ;
+        String line = null ;
+        StringBuffer sb = new StringBuffer() ;
+        boolean flag =false ;
+        while((line = br.readLine()) != null ){
+            if(line.contains("<script type=\"text/javascript\">") || flag){
+                sb.append(line) ;
+                flag = true ;
+            }
+            if(line.contains("</script>")){
+                flag =false ;
+                if(sb.length() > 0 ){
+                    if(sb.toString().contains("code_list") && sb.toString().contains("element_list")){
+                        break ;
+                    }else{
+                        sb.setLength(0) ;
+                    }
+                }
+            }
+        }
+        if(br != null ){
+            br.close() ;
+            br= null ;
+        }
+        System.out.println(sb.toString());
+        return sb.toString() ;
+    }
+
+    // 解析一组股票代码字符串   把code中包括的所有股票代码放入List中
+    public static List<String> handleStockCode(String code){
+        List<String> codes = null ;
+        int end = code.indexOf(";") ;
+        code = code.substring(0,end) ;
+        int start = code.lastIndexOf("=") ;
+        code = code.substring(start) ;
+        code = code.substring(code.indexOf("\"")+1,code.lastIndexOf("\"")) ;
+        codes = Arrays.asList(code.split(",")) ;
+        return codes ;
+    }
+
+
+
     public static void main(String[] args){
-        test();
+        //test();
+        try {
+            List<String> codes = new ArrayList<String>() ;
+            String batchstackcodes=StockTest.getBatchStackCodes(new URL("http://vip.stock.finance.sina.com.cn/q/go.php/vIR_CustomSearch/index.phtml?p=1"));
+            codes.addAll(StockTest.handleStockCode(batchstackcodes));
+            for(String temp:codes){
+                System.out.println(temp);
+            }
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
